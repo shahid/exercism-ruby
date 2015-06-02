7b353bb90703e5f276b96df96ba161098bcb7000
Shahid PA <shahid.azeez@gmail.com>
2015-06-02 14:23:42 +0530

    updated roman number exersices

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,29 +1,18 @@
 class Fixnum
   NUMBER_MAPPING = {
-    1000 => "M",  
-     900 => "CM",  
-     500 => "D",  
-     400 => "CD",
-     100 => "C",  
-      90 => "XC",  
-      50 => "L",  
-      40 => "XL",  
-      10 => "X",  
-        9 => "IX",  
-        5 => "V",  
-        4 => "IV",  
-        1 => "I",  
+    1000 => "M", 900 => "CM", 500 => "D", 400 => "CD", 100 => "C",  
+      90 => "XC", 50 => "L", 40 => "XL", 10 => "X", 9 => "IX",  
+        5 => "V", 4 => "IV", 1 => "I"
   }
   
   def to_roman
-    roman_numeral = ""
     arabic_numeral = self
-    NUMBER_MAPPING.each_pair do |arabic, roman|
-      while arabic_numeral >= arabic
-        arabic_numeral -= arabic
-        roman_numeral << roman
-      end
+
+    fail RangeError "Romans didn't use negative numbers #{self}" if self < 0    
+
+    NUMBER_MAPPING.inject('') do | acc, (num, roman)|
+      factor, arabic_numeral = arabic_numeral.divmod(num)
+      acc << roman * factor 
     end
-    roman_numeral
   end
 end
