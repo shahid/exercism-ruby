87e6235599886245405a12ace35dd204588edefa
Shahid PA <shahid.azeez@gmail.com>
2015-07-23 12:10:17 +0530

    Updated grains and leap year app

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,9 +1,9 @@
 class Grains
   def self.square(num)
-    return 2 ** (num-1)
+    2 ** (num-1)
   end
 
   def self.total
-    return 2 ** 64 - 1
+    2 ** 64 - 1
   end
 end
--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -1,5 +1,5 @@
 class Year
    def self.leap?(year)
-    (year % 400==0 || year%100!=0) && (year%4==0) ? true : false
+    (year % 400==0 || year%100!=0) && (year%4==0)
   end
 end
